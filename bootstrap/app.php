2959b3aab890842d73b541c305eb00bc8380f16e
scotch01 <arthuriascarlett@gmail.com>
2026-06-17 13:37:50 +0700

    fix redirect to admin panel from public page directly with login url

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'force.password' => \App\Http\Middleware\ForceChangePassword::class,
         ]);
+
+        $middleware->redirectUsersTo(function (Request $request) {
+
+            return route('admin.dashboard');
+
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
